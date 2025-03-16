0336b3152fd5a6aa9037fdc8bd2cfa6bba096f1e
sahil-singh1107 <sahilpython@gmail.com>
2025-03-16 19:34:39 +0530

    fix bugs

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import express from "express"
 import cors from "cors"
 import http from "http"
-import {Server} from "socket.io"
+import { Server } from "socket.io"
 import dotenv from "dotenv"
 import fs from "fs"
-import {Readable} from "stream"
+import { Readable } from "stream"
 import axios from "axios"
-import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3"
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import path from "path"
 
 dotenv.config();
 
 const s3 = new S3Client({
     credentials: {
-        accessKeyId : process.env.ACCESS_KEY,
-        secretAccessKey : process.env.SECRET_KEY
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY
     },
-    region : process.env.BUCKET_REGION
+    region: process.env.BUCKET_REGION
 })
 
 const app = express();
@@ -25,9 +26,9 @@ const server = http.createServer(app);
 app.use(cors())
 
 const io = new Server(server, {
-    cors : {
-        origin : process.env.ELECTRON_HOST,
-        methods : ["GET", "POST"]
+    cors: {
+        origin: process.env.ELECTRON_HOST,
+        methods: ["GET", "POST"]
     }
 })
 
@@ -38,10 +39,14 @@ io.on("connection", (socket) => {
 
     socket.on("video-chunks", async (data) => {
         console.log("🟢 video chunk is sent", data);
-        const writeStreams = fs.createWriteStream("temp_upload/"+data.filename)
+        const uploadDir = path.join(__dirname, "temp_upload");
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+        const writeStreams = fs.createWriteStream("temp_upload/" + data.filename)
         recordedChunks.push(data.chunks)
         const videoBlob = new Blob(recordedChunks, {
-            type : "video/webm; codecs=vp9"
+            type: "video/webm; codecs=vp9"
         })
         const buffer = Buffer.from(await videoBlob.arrayBuffer());
         const readStream = Readable.from(buffer);
@@ -54,9 +59,9 @@ io.on("connection", (socket) => {
         console.log("🟢 processing video");
 
         recordedChunks = [];
-        fs.readFile("temp_upload/" + data.filename, async (err,file) => {
+        fs.readFile("temp_upload/" + data.filename, async (err, file) => {
             const processing = await axios.post(`${process.env.NEXT_API_HOST}recording/${data.userId}/processing`, {
-                filename : data.filename
+                filename: data.filename
             })
 
             //console.log("processing", processing);
@@ -69,20 +74,20 @@ io.on("connection", (socket) => {
             const Bucket = process.env.BUCKET_NAME
             const ContentType = "video/webm"
             const command = new PutObjectCommand({
-                Key : key,
+                Key: key,
                 Bucket,
                 ContentType,
-                Body : file
+                Body: file
             })
 
             const fileStatus = await s3.send(command);
 
             if (fileStatus['$metadata'].httpStatusCode === 200) {
-                console.log("🟢 video uploaded to aws");    
+                console.log("🟢 video uploaded to aws");
             }
 
             const stopprocessing = await axios.post(`${process.env.NEXT_API_HOST}recording/${processing.data.videoId}/complete`, {
-                filename : data.filename
+                filename: data.filename
             })
 
             if (stopprocessing.data.status !== 200) {
@@ -93,11 +98,11 @@ io.on("connection", (socket) => {
                     if (!err) {
                         console.log(data.filename + " " + "deleted successfully.");
                     }
-                    
+
                 })
             }
         })
-    
+
     })
 
     socket.on("disconnect", async (data) => {
